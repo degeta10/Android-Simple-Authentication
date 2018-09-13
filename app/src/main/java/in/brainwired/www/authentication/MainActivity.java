6704d5cd664e0b066bb857f67e2b69d9c580ec49
Devender Gupta <degeta10@gmail.com>
2018-09-13 18:03:19 +0530

    Added Logout option

--- app/src/main/java/in/brainwired/www/authentication/MainActivity.java
+++ app/src/main/java/in/brainwired/www/authentication/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -24,10 +26,6 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextInputLayout register_Username,register_Password,register_Email;
-    private Button register_Button,login_Button;
-    private ProgressDialog dialog;
-
     private TextView username_Text;
 
     @Override
@@ -44,28 +42,6 @@ public class MainActivity extends AppCompatActivity {
         username_Text=findViewById(R.id.username);
         username_Text.setText(SharedPrefManager.getInstance(this).getUsername());
 
-//        register_Username = findViewById(R.id.register_username);
-//        register_Email = findViewById(R.id.register_email);
-//        register_Password = findViewById(R.id.register_password);
-//
-//        register_Button=findViewById(R.id.register_button);
-//        login_Button=findViewById(R.id.login_button);
-//        dialog = new ProgressDialog(this);
-//
-//        register_Button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                registerUser();
-//            }
-//        });
-//
-//        login_Button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent login_intent = new Intent(MainActivity.this,LoginActivity.class);
-//                startActivity(login_intent);
-//            }
-//        });
     }
 
     @Override
@@ -78,45 +54,22 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-//    private void registerUser() {
-//        final String username = register_Username.getEditText().getText().toString();
-//        final String email = register_Email.getEditText().getText().toString();
-//        final String password = register_Password.getEditText().getText().toString();
-//
-//        dialog.setMessage("Registering User...");
-//        dialog.show();
-//        StringRequest request = new StringRequest(Request.Method.POST, Constants.REGISTER_URL,
-//                new Response.Listener<String>() {
-//                    @Override
-//                    public void onResponse(String response) {
-//                        dialog.dismiss();
-//                        try {
-//                            JSONObject jsonObject= new JSONObject(response);
-//                            Toast.makeText(MainActivity.this,jsonObject.getString("message"),Toast.LENGTH_SHORT).show();
-//                        } catch (JSONException e) {
-//                            Toast.makeText(MainActivity.this,"JSON Error",Toast.LENGTH_SHORT).show();
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                },
-//                new Response.ErrorListener() {
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {
-//                        dialog.hide();
-//                        Toast.makeText(MainActivity.this,error.getMessage(),Toast.LENGTH_SHORT).show();
-//                    }
-//                }){
-//            @Override
-//            protected Map<String, String> getParams() throws AuthFailureError {
-//                Map<String,String> params = new HashMap<>();
-//                params.put("username",username);
-//                params.put("email",email);
-//                params.put("password",password);
-//                return params;
-//            }
-//        };
-//
-//        RequestHandler.getInstance(this).addToRequestQueue(request);
-//
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_layout,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.menuLogout:
+                                SharedPrefManager.getInstance(this).LogOut();
+                                finish();
+                                startActivity(new Intent(this,LoginActivity.class));
+                                break;
+        }
+        return true;
+    }
 }
